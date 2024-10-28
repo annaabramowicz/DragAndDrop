e5b4bc482f99028db5d71fdd41a570d944d90fcc
Anna Abramowicz <42539465+annaabramowicz@users.noreply.github.com>
2024-10-28 14:21:47 +0100

    basic style

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import "./reset.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="drag-and-drop">
+        <div className="dnd-group">
+          <div className="dnd-item">
+            <div>
+              <p>ITEM 1</p>
+            </div>
+          </div>
+        </div>
+        <div className="dnd-group">
+          <div className="dnd-item">
+            <div>
+              <p>ITEM 2</p>
+            </div>
+          </div>
+        </div>
+        <div className="dnd-group">
+          <div className="dnd-item">
+            <div>
+              <p>ITEM 3</p>
+            </div>
+          </div>
+        </div>
+        <div className="dnd-group">
+          <div className="dnd-item">
+            <div>
+              <p>ITEM 4</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
